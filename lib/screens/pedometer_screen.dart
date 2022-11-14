ed3932fd0a0f084ed2d491d953f07a2853db50cf
syukri anuar <muhamadsyukrianuar@gmail.com>
2022-11-14 08:21:44 +0800

    first commit

---
+++ lib/screens/pedometer_screen.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:pedometer/pedometer.dart';
+
+class PedometerScreen extends StatefulWidget {
+  const PedometerScreen({super.key});
+
+  @override
+  State<PedometerScreen> createState() => _PedometerScreenState();
+}
+
+class _PedometerScreenState extends State<PedometerScreen> {
+  late Stream<StepCount> _stepCountStream;
+  late Stream<PedestrianStatus> _pedestrianStatusStream;
+  late String _status = '';
+  late String _steps = '';
+
+  @override
+  void initState() {
+    // _stepCountStream;
+    // _pedestrianStatusStream;
+    // _status;
+    // _steps;
+    initPlatformState();
+    super.initState();
+  }
+
+  void onStepCount(StepCount event) {
+    if (kDebugMode) {
+      print(event);
+    }
+    setState(() {
+      _steps = event.steps.toString();
+    });
+  }
+
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    if (kDebugMode) {
+      print(event);
+    }
+    setState(() {
+      _status = event.status;
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    if (kDebugMode) {
+      print('onPedestrianStatusError: $error');
+    }
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    if (kDebugMode) {
+      print(_status);
+    }
+  }
+
+  void onStepCountError(error) {
+    if (kDebugMode) {
+      print('onStepCountError: $error');
+    }
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  Future<void> initPlatformState() async {
+    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+    _pedestrianStatusStream
+        .listen(onPedestrianStatusChanged)
+        .onError(onPedestrianStatusError);
+
+    if (kDebugMode) {
+      print(_steps);
+      print(_status);
+    }
+    
+
+    if (!mounted) return;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Pedometer example app'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Steps taken:',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                _steps,
+                style: const TextStyle(fontSize: 60),
+              ),
+              const Divider(
+                height: 100,
+                thickness: 0,
+                color: Colors.white,
+              ),
+              const Text(
+                'Pedestrian status:',
+                style: TextStyle(fontSize: 30),
+              ),
+              Icon(
+                _status == 'walking'
+                    ? Icons.directions_walk
+                    : _status == 'stopped'
+                        ? Icons.accessibility_new
+                        : Icons.error,
+                size: 100,
+              ),
+              Center(
+                child: Text(
+                  _status,
+                  style: _status == 'walking' || _status == 'stopped'
+                      ? const TextStyle(fontSize: 30)
+                      : const TextStyle(fontSize: 20, color: Colors.red),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
